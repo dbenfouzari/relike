d98028f146c138e75cf1f715050419330ac18014
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-19 19:33:32 +0200

    :white_check_mark: Setup tests from root

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    "<rootDir>/packages/(?:.+?)/dist/",
+    "<rootDir>/(.*)/node_modules",
+  ],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "babel-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/test/testSetupFile.js"],
+  testEnvironment: "jsdom",
+};
