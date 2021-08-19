4885950dd56f118044def8981b880b3419cd52d5
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-19 14:10:44 +0200

    feat(components): init package with create-react-app

--- packages/client/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { helloWorld, goodByeWorld } from '@fastics/server';
-
-console.log(helloWorld('Donovan'));
-console.log(goodByeWorld('Donovan'));
--- packages/server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { nanoid } from 'nanoid';
-
-export function helloWorld(name: string) {
-  return `Hello World to ${name} with uniqueID ${nanoid()}`;
-}
-
-export function goodByeWorld(name: string) {
-  return `Good bye ${name} !`;
-}
