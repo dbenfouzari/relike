722a935c501f14c783586c0ef5c554e9f98fe52a
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-09-06 17:21:00 +0200

    test(avatar): fix avatar tests

--- packages/components/src/avatar/avatar.test.tsx
+++ packages/components/src/avatar/avatar.test.tsx
@@ -4,7 +4,11 @@ import Avatar from "./avatar";
 
 describe("Avatar", () => {
   it("should render successfully", () => {
-    const { container } = render(<Avatar />);
+    const { container } = render(
+      <Avatar>
+        <span>M</span>
+      </Avatar>,
+    );
 
     expect(container.firstChild).toMatchSnapshot();
   });
