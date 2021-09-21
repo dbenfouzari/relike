ecb900c6af24b87fd93d59bc5d030d46cc963b8d
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-09-21 16:45:28 +0200

    test(alert): better alert coverage

--- packages/components/src/alert/alert.test.tsx
+++ packages/components/src/alert/alert.test.tsx
@@ -1,11 +1,28 @@
 import { render } from "@testing-library/react";
 
 import Alert from "./alert";
+import { AlertSeverities } from "./constants";
 
 describe("Alert", () => {
   it("should render successfully", () => {
     const { container } = render(<Alert />);
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should render with function children", () => {
+    const { getByTestId } = render(
+      <Alert severity={AlertSeverities.error} title="Test">
+        {({ color }) => (
+          <div data-testid="content" style={{ backgroundColor: color.toRGB() }}>
+            <h1>Hello World</h1>
+          </div>
+        )}
+      </Alert>,
+    );
+
+    const style = window.getComputedStyle(getByTestId("content"));
+
+    expect(style.backgroundColor).toEqual("rgb(95, 33, 32)");
   });
 });
