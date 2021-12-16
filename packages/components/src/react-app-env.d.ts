8ca53bea0f234cf63b36bbf375a4b2fa3be0d76a
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-12-16 11:18:10 +0100

    chore(eslint): fix padding lint

--- packages/components/src/react-app-env.d.ts
+++ packages/components/src/react-app-env.d.ts
@@ -3,8 +3,11 @@
 import "react";
 
 declare module "react" {
+  /** This overrides HTMLAttributes to allow `react-jss` special props */
   interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    /** Is the style used in a JSX component */
     jsx?: boolean;
+    /** Is the style global */
     global?: boolean;
   }
 }
