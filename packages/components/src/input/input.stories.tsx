8cbc86171d42643ee18db49ef6bc72ec5f816a92
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-12-17 11:32:42 +0100

    chore(eslint): fix input lint

--- packages/components/src/input/input.stories.tsx
+++ packages/components/src/input/input.stories.tsx
@@ -1,6 +1,6 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Meta, Story } from "@storybook/react";
 
-import Input from "./input";
+import Input, { InputProps } from "./input";
 import useTextEditingController from "./useTextEditingController";
 
 export default {
@@ -9,15 +9,28 @@ export default {
   argTypes: {
     controller: { control: { disable: true } },
   },
-} as ComponentMeta<typeof Input>;
+} as Meta<InputProps>;
 
-const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+/**
+ * Default template
+ *
+ * @param {InputProps} args The props.
+ * @example
+ * <Template />
+ * @returns {JSX.Element} The template
+ */
+const Template: Story<InputProps> = (args) => <Input {...args} />;
 
 /**
  * Controlling [Input] may be useful to get current input value.
  * You can get it by calling `getValue` method, or by passing `listenValue` option to `true` then calling `.value`.
+ *
+ * @param {InputProps} args The props.
+ * @example
+ * <TemplateControlled />
+ * @returns {JSX.Element} The template
  */
-const TemplateControlled: ComponentStory<typeof Input> = (args) => {
+const TemplateControlled: Story<InputProps> = (args) => {
   const _controller = useTextEditingController({ listenValue: true });
 
   return (
