c24ded72a01b1e60e2b85d1bf00050aaf42cfd9c
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-12-30 17:44:57 +0100

    test(stepper-item): improve stepper-item coverage

--- packages/components/src/stepper/components/stepper-item/stepper-item.test.tsx
+++ packages/components/src/stepper/components/stepper-item/stepper-item.test.tsx
@@ -1,21 +1,50 @@
 import { render, screen } from "@testing-library/react";
 
 import Colors from "../../../colors";
-import { InternalStepperItem } from "./stepper-item";
+import { InternalStepperItem, InternalStepperItemProps, StepperItem } from "./stepper-item";
 
-it("should render successfully", () => {
-  render(
-    <InternalStepperItem
-      title="Test"
-      isCurrent
-      isPast={false}
-      titleColor={Colors.blue}
-      primaryColor={Colors.blue}
-      secondaryColor={Colors.red}
-      disabledColor={Colors.grey}
-      index={1}
-    />,
-  );
+/**
+ * Set up InternalStepperItem tests
+ *
+ * @param   {Partial<InternalStepperItemProps>} props The props
+ * @returns {void}                                    The result
+ * @example
+ * setup()
+ */
+const setup = (props: Partial<InternalStepperItemProps> = {}) => {
+  const mergedProps: InternalStepperItemProps = {
+    title: "Test",
+    isCurrent: true,
+    isPast: false,
+    titleColor: Colors.blue,
+    primaryColor: Colors.blue,
+    secondaryColor: Colors.red,
+    disabledColor: Colors.grey,
+    index: 1,
+    ...props,
+  };
 
-  expect(screen.getByRole("listitem")).toMatchSnapshot();
+  render(<InternalStepperItem {...mergedProps} />);
+};
+
+describe("Components/StepperItem", () => {
+  it("StepperItem should render null", () => {
+    const { container } = render(<StepperItem title="Zbra" />);
+
+    // Disable ESLint because I want to test that it renders nothing.
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.firstChild).toBe(null);
+  });
+
+  it("should render successfully", () => {
+    setup();
+
+    expect(screen.getByRole("listitem")).toMatchSnapshot();
+  });
+
+  it("should render successfully when is past", () => {
+    setup({ isPast: true });
+
+    expect(screen.getByRole("listitem")).toMatchSnapshot();
+  });
 });
